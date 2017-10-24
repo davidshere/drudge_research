8073e106c83ed5a6c4ec24714debbf5c3934148b
davidshere <sheredlk@gmail.com>
2017-10-23 23:33:36 -0400

    lots of cleanup

--- scraper/models.py
+++ scraper/models.py
@@ -13,7 +13,6 @@ import retrying
 from parse_main_and_splash import parse_main_and_splash
 
 DAY_PAGE_FMT_URL = "http://www.drudgereportarchives.com/data/%s/%02d/%02d/index.htm?s=flag"
-DRUDGE_PAGE_URL_PREFIX = 'http://www.drudgereportArchives.com/data/'
 
 DRUDGE_LINK_FIELD_NAMES = ['page_dt', 'url', 'hed', 'is_top', 'is_splash']
 DrudgeLink = collections.namedtuple("DrudgeLink", DRUDGE_LINK_FIELD_NAMES)
@@ -21,20 +20,17 @@ DrudgeLink = collections.namedtuple("DrudgeLink", DRUDGE_LINK_FIELD_NAMES)
 SEMAPHORE_COUNT = 5
 PROCESS_COUNT = multiprocessing.cpu_count()
 
+# Set up logging. This can't be the best way...
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
-
 logger.addHandler(ch)
 
 class FetchError(Exception):
     pass
-
-
 
 class DrudgePage(object):
     """ Represents one an individual snapshot of the Drudge Report """
@@ -82,23 +78,51 @@ class DayPage(object):
     this: http://www.drudgereportarchives.com/data/2017/10/02/index.htm?s=flag
 
     Handles all the behavior of asynchronously fetching the Drudge snapshots
-    and returning them.
+    and scheduling them to be processed.
     """
     def __init__(self, dt, drudge_page_limit=None):
-        self.loop = asyncio.get_event_loop()
-        self.loop.set_debug(True)
+        self._loop = asyncio.get_event_loop()
+        self._loop.set_debug(True)
 
         self.dt = dt
         self.drudge_page_limit = drudge_page_limit
         self.url = DAY_PAGE_FMT_URL % (dt.year, dt.month, dt.day)
 
-    def get_day_page(self):
+    def _prep_queues(self):
+        """
+        This method instantiates the queues and processes that we'll use
+        to handle turning the fetched HTML into lists of links.
+        """
+        self._task_queue = multiprocessing.JoinableQueue()
+        self._processed_page_queue = multiprocessing.Queue()
+
+        self._consumers = [
+            DrudgePageScrapeHandler(self._task_queue, self._processed_page_queue)
+            for _ in range(PROCESS_COUNT)
+        ]
+        for proc in self._consumers:
+            proc.start()
+
+    def _processed_page_queue_to_links(self):
+        """
+        Takes a queue filled with lists of DrudgeLinks and returns
+        a single list of links
+        """
+        self._processed_page_queue.put(None)
+        links = []
+        for i in iter(self._processed_page_queue.get, None):
+            links.extend(i)
+        return links
+
+    def _get_day_page(self):
+        """ Fetches HTML for a Day Page """
         response = requests.get(self.url)
 
         logger.info("Fetched Day Page for %s", self.dt.isoformat())
         return BeautifulSoup(response.content, 'lxml')
 
-    def drudge_page_from_drudge_page_url(self, link):
+    def _drudge_page_from_drudge_page_url(self, link):
+        """ Transforms a URL into a DrudgePage object """
         url = link.get('href')
         if url:
             url_dt = url.split('/')[-1]
@@ -108,108 +132,90 @@ class DayPage(object):
                 page_dt=page_dt
             )
 
-    def get_one_days_drudge_pages(self):
-
+    def _get_one_days_drudge_pages(self, page_limit=None):
+        """
+        Transforms a Day Page's HTML into a list of links
+        to individual drudge pages. Returns a list of
+        DrudgePage objects.
+        """
         all_links = self.day_page.find_all('a')
         links = []
 
         if self.drudge_page_limit:
-            all_links = all_links[:self.drudge_page_limit]
+            all_links = all_links[:page_limit]
 
         for link in all_links:
             try:
-                page = self.drudge_page_from_drudge_page_url(link)
+                page = self._drudge_page_from_drudge_page_url(link)
                 if page:
-                    self.tasks.put(page)
                     links.append(page)
             except ValueError:
                 pass
 
         return links
 
-    async def bound_fetch_drudge_page(self, page, sem, session):
-        async with sem:
-            page.html = await self.fetch_drudge_page(page.url, session)
-            self.tasks.put(page)
-            return page
+    async def _fetch_drudge_page(self, page, sem, session):
+        logger.info("Fetching drudge page at %s", page.url)
 
-    async def fetch_drudge_page(self, url, session):
-        async with session.get(url) as response:
-            logger.info("Fetching drudge page at %s", url)
-
-            # this returns a list, we only want one item
+        async with sem, session.get(page.url) as response:
             response = await asyncio.gather(response.read())
-            response = response[0]
-            #logger.info("return \n%s" % response[:10])
-            #logger.info("return \n%s" % type(response))
-            return response
 
+        page.html = response[0]
+        self._task_queue.put(page)
 
-
-    async def fetch_drudge_page_tasks(self):
+    async def _generate_fetch_tasks(self):
         tasks = []
         sem = asyncio.Semaphore(SEMAPHORE_COUNT)
 
         # Create client session that will ensure we dont open new connection
         # per each request.
         async with ClientSession() as session:
-            pages = self.get_one_days_drudge_pages()
+            pages = self._get_one_days_drudge_pages(self.drudge_page_limit)
             for page in pages:
-                task = asyncio.ensure_future(self.bound_fetch_drudge_page(page, sem, session))
-                print(task)
+                task = asyncio.ensure_future(self._fetch_drudge_page(page, sem, session))
                 tasks.append(task)
-            #return await tasks
-
             return await asyncio.gather(*tasks)
 
     #@retrying.retry(retry_on_exception=lambda exception: isinstance(exception, FetchError), stop_max_attempt_number=3)
     def process_day(self):
-        self.tasks = multiprocessing.JoinableQueue()
-
-        results = multiprocessing.Queue()
-        self.consumers = [DrudgePageScrapeHandler(self.tasks, results) for _ in range(PROCESS_COUNT)]
-
-        for proc in self.consumers:
-            proc.start()     
+        """
+        Primary public method. Used to fetch one day's worth of links
+        from the drudge archive.
+        """
+        # instantiate queues and processes for processing the results
+        self._prep_queues()
 
         logger.info("Fetching Day Page for %s, url: %s", self.dt.isoformat(), self.url)
 
-        fetch_start = time.time()
+        # schedule async fetch tasks
         try:
-            self.day_page = self.get_day_page()
-            future = asyncio.ensure_future(self.fetch_drudge_page_tasks())
-            self.loop.run_until_complete(future)
-
-
+            self.day_page = self._get_day_page()
+            future = asyncio.ensure_future(self._generate_fetch_tasks())
+            self._loop.run_until_complete(future)
         except ClientError as e:
             logger.error("Failure on %s, url: %s", self.dt.isoformat(), self.url)
             raise FetchError
 
-
+        # add poison pills so the queue will know when to stop
         for _ in range(PROCESS_COUNT):
-            self.tasks.put(None)
+            self._task_queue.put(None)
 
-        self.tasks.join()
+        # block execution until the queues are empty
+        self._task_queue.join()
+        links = self._processed_page_queue_to_links()
 
-
-
-
-
-        # dump queue to list
-
-        # add a poison pill
-        results.put(None)
-        links = []
-        for i in iter(results.get, None):
-            links.extend(i)
-
-        for proc in self.consumers:
+        # turn off processes
+        for proc in self._consumers:
             proc.terminate()
-
 
         return links
 
 class DrudgePageScrapeHandler(multiprocessing.Process):
+    """
+    This class does one thing - pops a Drudge Page off
+    of a task queue, processes it, and puts the result
+    into a result queue.
+    """
 
     def __init__(self, task_queue, result_queue):
         multiprocessing.Process.__init__(self)
