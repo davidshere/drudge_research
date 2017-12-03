97621501db550cc42a9b05ebb3df23c6591f970d
davidshere <sheredlk@gmail.com>
2017-12-03 15:42:27 -0500

    update transform to use sets and allow for multiple splash headlines

--- scraper/test_splash.py
+++ scraper/test_splash.py
@@ -1,7 +1,7 @@
 import datetime
 import unittest
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 
 from transform_page_into_drudge_links import *
 
@@ -11,71 +11,75 @@ LATE_DT = datetime.datetime(2017, 1, 1, 0, 0)
 #TODO: The keys here should be datetimes, not numbers. That will require going and finding each page again in the archive
 EXPECTED_RESULTS = {
   0: {
-    'splash': None,
-    'top': []
+    'splash': set(),
+    'top': set()
   },
   1: {
-    'splash': 'ASHCROFT TO REQUIRE SOME VISITORS FINGERPRINTED',
-    'top': []
+    'splash': {'ASHCROFT TO REQUIRE SOME VISITORS FINGERPRINTED'},
+    'top': {
+      'India magazine paints scenario of horror...',
+      'NUCLEAR RIVALS EXCHANGE INSULTS... ',
+      'German delay threatens Meteor missile...'
+    }
   },
   2: {
-    'splash': None,
-    'top': []
+    'splash': set(),
+    'top': set()
   },
   3: {
-    'splash': 'CBS: BUSH KNEW IRAQ INFO WAS FALSE',
-    'top': []
+    'splash': {'CBS: BUSH KNEW IRAQ INFO WAS FALSE'},
+    'top': set()
   },
   4: {
-    'splash': 'New worm spreading rapidly across Internet',
-    'top': ['Dow Hits 31-Month High...']
+    'splash': {'New worm spreading rapidly across Internet'},
+    'top': {'Dow Hits 31-Month High...'}
   },
   5: {
-    'splash': 'HELL STORM SLAMS COAST; WINDS TOP 145 MPH',
-    'top': []
+    'splash': {'HELL STORM SLAMS COAST; WINDS TOP 145 MPH'},
+    'top': set()
   },
   6: {
-    'splash': None,
-    'top': [
+    'splash': set(),
+    'top': {
       "FLASHBACK:  Judge in charge of Saddam Hussein's trial was in fear for his life today after his identity was revealed by a UK newspaper..."
-    ]
+    }
   },
   7: {
-    'splash': 'Relief Head Questions New Orleans Reopen',
-    'top': [
+    'splash': {'Relief Head Questions New Orleans Reopen'},
+    'top': {
       'Adios, Posse Comitatus:  Military May Play Bigger Relief Role...',
       'Money Earmarked for Evacuation Was Redirected...',
       'Dem Brazile:  I Will Rebuild With You, Mr. President...'
-    ]
+    }
   },
   8: {
-    'splash': 'Bird flu confirmed:  Dead swan in the UK',
-    'top': [
+    'splash': {'Bird flu confirmed:  Dead swan in the UK'},
+    'top': {
       "Clinton Deja Vu: Hillary Caught Stealing Bill's Old Lines... ",
       'Sen. Clinton says immigration bill would make her a criminal...'
-    ]
+    }
   },
   9: {
-    'splash': None,
-    'top': [
+    'splash': set(),
+    'top': {
       'DRUDGE RADIO LIVE SUNDAY NIGHT 10 PM ET TO 1 AM... HEARD IN ALL 50 STATES...'
-    ]
+    }
   },
   10: {
-    'splash': 'STALEMATE',
-    'top': [
+    'splash': {'STALEMATE'},
+    'top': {
       'BIGGEST GOVERNMENT   EVER!  Fed revenue collections and spending  at all-time highs... MORE...'
-    ]
+    }
   },
   11: {
-    'splash': 'TROUBLE IN THE CITI',
-    'top': [
+    'splash': {'TROUBLE IN THE CITI'},
+    'top': {
       "CAN SHE BEAT THEM?  New poll shows Clinton trails top '08 Republicans..."
-    ]
+    }
   },
   12: {
-    'splash': 'RUSSERT DEAD AT NBC',
-    'top': [
+    'splash': {'RUSSERT DEAD AT NBC'},
+    'top': {
       'Tributes pour in...',
       'Collapsed in the office...',
       'Last interview...',
@@ -83,79 +87,79 @@ EXPECTED_RESULTS = {
       'Live Coverage...',
       'Towering figure in American journalism...',
       'Industry Shock...'
-    ]
+    }
   },
   13: {
-    'splash': 'BLAGO STRIKES BACK!',
-    'top': [
+    'splash': {'BLAGO STRIKES BACK!'},
+    'top': {
       'On collision course with Washington...',
       'CHICAGOLAND:  Indicted Governor Names Obama Replacement...'
-    ]
+    }
   },
   14: {
-    'splash': 'THE WAY IT USED TO BE',
-    'top': [
+    'splash': {'THE WAY IT USED TO BE'},
+    'top': {
       'Taliban video shows captive US soldier...'
-    ]
+    }
   },
   15: {
-    'splash': 'SEAT ME',
-    'top': [
+    'splash': {'SEAT ME'},
+    'top': {
         'COURIC FACES PAY CUT; DEEP LAYOFFS HIT CBSNEWS...',
         'NY POST:  COURIC, CBS REACH CROSSROADS...',
         "'This is where I am for now'..."
-      ]
+      }
     },
   16: {
-    'splash': 'PENTAGON EYES WIKILEAKS CHARGES',
-    'top': [
+    'splash': {'PENTAGON EYES WIKILEAKS CHARGES'},
+    'top': {
       'Iran unveils long-range bombing drone...',
       'US delays missile-zapping laser test for 4th time...'
-    ]
+    }
   },
   17: {
-    'splash': 'SAUDI POLICE OPEN FIRE ON PROTESTERS',
-    'top': [
+    'splash': {'SAUDI POLICE OPEN FIRE ON PROTESTERS'},
+    'top': {
       'SARKOZY, CAMERON TAKE CHARGE ON LIBYA...',
       'HILLARY: U.S. should wait for world to act...',
       'Top U.S. Spy: GADHAFI WILL PREVAIL...'
-    ]
+    }
   },
   18: {
-    'splash': 'OBAMACARE HEADED FOR SUPREMES',
-    'top': [
+    'splash': {'OBAMACARE HEADED FOR SUPREMES'},
+    'top': {
       '$200K Per Job? Geithner Says White House  Plan Still Bargain...'
-    ]
+    }
   },
   19: {
-    'splash': 'ROMNEY WARNS:  OBAMA COMING FOR GUNS',
-    'top': [
+    'splash': {'ROMNEY WARNS:  OBAMA COMING FOR GUNS'},
+    'top': {
       'WHAT HAPPENS IN VEGAS, STAYS IN VEGAS:  GSA OFFICIAL TO THE FIFTH...'
-    ]
+    }
   },
   20: {
-    'splash': 'THE MORNING AFTER', 
-    'top': []
+    'splash': {'THE MORNING AFTER'},
+    'top': set()
   },
   21: {
-    'splash': "TAXES IN FRANCE 'TOP 100% OF INCOME'",
-    'top': []
+    'splash': {"TAXES IN FRANCE 'TOP 100% OF INCOME'"},
+    'top': set()
   },
   22: {
-    'splash': 'MEX NUKE MYSTERY',
-    'top': [
+    'splash': {'MEX NUKE MYSTERY'},
+    'top': {
       'BLAST:  -40\xb0 MIDWEST!',
       'Deep freeze across western half of nation...',
       'Chill Temps Live...'
-    ]
+    }
   },
   23: {
-    'splash': 'BOOK: HILLARY HAS BAD HEART',
-    'top': []
+    'splash': {'BOOK: HILLARY HAS BAD HEART'},
+    'top': set()
   },
   24: {
-    'splash': 'EURABIA DEBATE ERUPTS',
-    'top': [
+    'splash': {'EURABIA DEBATE ERUPTS'},
+    'top': {
       'Suspects spotted, manhunt continues...',
       'Kouachi Rap Video... ',
       'Police find abandoned car with Molotov cocktails, Jihadist flags...',
@@ -173,30 +177,42 @@ EXPECTED_RESULTS = {
       'Muslim Cleric Defends Slaughter... ',
       'Islamic immigration to USA on rise... ',
       "Britain's MI5 warns al Qaeda planning mass attacks on West..."
-    ]
+    }
   },
   0.5: {
-    'splash': None,
-    'top': [
+    'splash': set(),
+    'top': {
       "Gen. Wesley Clark: 'White House tried to get me knocked off CNN'..."
-    ]
+    }
   },
   1.5: {
-    'splash': 'WHAT WILL IT BE?',
-    'top': [
+    'splash': {'WHAT WILL IT BE?'},
+    'top': {
       'Murdoch rebuffs level of DOW JONES family support...',
       'Down to the Wire... '
-    ],
+    },
     'raises': ParseError
+  },
+  25: {
+    'splash': {'JARED SPEAKS!PUBLIC TALK ON MIDEAST PEACE', 'VIDEO'},
+    'top': {
+      'REVEALED:  TRUMP HATER ON MUELLER TEAM...',
+      'Special Counsel Removes Top Aide After Texts...',
+      '*Role in Clinton email investigation under review...',
+      "Flynn's plea fails to reveal smoking gun...",
+      'Inside secretive nerve center of investigation...',
+      'House Republicans Prepare Contempt Action Against FBI, DOJ...',
+      "PRESIDENT'S GALLUP POLL PLUNGE:  62% DISAPPROVAL..."
+    }
   }
 }
 
 def top_splash_to_text(top_and_splash):
   # don't want to test this
   if top_and_splash['splash']:
-    top_and_splash['splash'] = top_and_splash['splash'].text
+    top_and_splash['splash'] = {tag.text for tag in top_and_splash['splash'] if isinstance(tag, Tag)}
   if top_and_splash['top']:
-    top_and_splash['top'] = [a.text for a in top_and_splash['top']]
+    top_and_splash['top'] = {a.text for a in top_and_splash['top'] if isinstance(a, Tag)}
   return top_and_splash
 
 def load_resource(page_number):
@@ -208,6 +224,7 @@ def load_resource(page_number):
 class TopAndSplashTest(unittest.TestCase):
 
   def test_parser(self):
+    self.maxDiff=None
     for file_number in EXPECTED_RESULTS.keys():
       # need to simulate the datetimes that we're getting from a DayPage
       file_dt = EARLY_DT if file_number <= 14 else LATE_DT
--- scraper/transform_page_into_drudge_links.py
+++ scraper/transform_page_into_drudge_links.py
@@ -7,7 +7,8 @@ import urllib.parse
 STOP_DOMAINS = [
   'harvest.adgardener.com',
   'a.tribalfusion.com',
-  'www.medintop.com'
+  'www.medintop.com',
+  'www.drudgereportarchives.com'
 ]
 
 LOGO_FILENAME = 'logo9.gif'
@@ -27,7 +28,7 @@ class ParseError(Exception):
 
 # before mid-2009
 def find_splash_with_font_size(soup):
-  """ Takes soup and returns the <a> element representing
+  """ Takes soup and returns the <a> elements representing
       the page splash.
 
       If no link element is found, returns None
@@ -36,15 +37,15 @@ def find_splash_with_font_size(soup):
   if not font_size_element:
     return None
 
-  # if the HTML is malformed, this will return None
-  link = font_size_element.find('a')
-  if link:
-    return link
+  # if the HTML is malformed or the headline isn't a link, this will return None
+  splash_links = font_size_element.find_all('a')
+  if splash_links:
+    return set(splash_links)
 
   # returns -1 if it finds nothing
   link = font_size_element.next.find('a')
   if link != -1:
-    return link
+    return set([link])
 
 
 def is_logo_link(element):
@@ -63,7 +64,7 @@ def is_logo_link(element):
   return src.split('/')[-1] == LOGO_FILENAME
 
 
-def index_for_splash_element_in_list_of_liks(found_splash, links):
+def index_for_splash_element_in_list_of_links(found_splash, links):
   """ Find the index of the element found_splash in the list of links. """
   if found_splash and found_splash in links:
     splash_index = links.index(found_splash)
@@ -73,21 +74,31 @@ def index_for_splash_element_in_list_of_liks(found_splash, links):
         splash_index = index
         break
     else:
-      raise ParseError("Couldn't parse metadata for {}, HTML likely malformed.".format(page_dt))
+      raise ParseError("Couldn't parse metadata for {}, HTML likely malformed.")
+
+  return splash_index
 
 
 def get_early_top(links, found_splash, page_dt):
-  top_links = []
+  found_splash = found_splash or set()
+  top_links = set()
 
-  splash_index = index_for_splash_element_in_list_of_liks(found_splash, links)
+  try:
+    splash_index = index_for_splash_element_in_list_of_links(found_splash, links)
+  except ParseError as e:
+    error_message = e.args[0]
+    if '{}' in error_message:
+      raise ParseError(error_message.format(page_dt))
+    else:
+      raise
 
   for link in links[splash_index-1 : 0 : -1]:
     href_netloc = urllib.parse.urlparse(link.get('href')).netloc.lower()
     if href_netloc in STOP_DOMAINS:
       return top_links
 
-    if link.text:
-      top_links.append(link)
+    if link.text and link not in found_splash:
+      top_links.add(link)
   raise ParseError("get_early_top returned None - it shouldn't do that")
 
 
@@ -95,18 +106,25 @@ def get_early_top(links, found_splash, page_dt):
 def early_top_splash_finder(soup, page_dt):
   splash = find_splash_with_font_size(soup)
   links = soup.find_all('a')
-  top = get_early_top(links, splash, page_dt) or []
-  return {'top': top, 'splash': splash}
+  top = get_early_top(links, splash, page_dt)
+  return {
+    'top': top or set(),
+    'splash': splash or set()
+  }
 
 
 # going back to mid-2009
 def recent_top_splash_finder(soup):
   drudge_top_headlines = soup.find('div', {'id': 'drudgeTopHeadlines'})
+
   if not drudge_top_headlines:
     raise ParseError("id: 'drudgeTopHeadlines' not found")
 
+  splash = find_splash_with_font_size(drudge_top_headlines)
+
   top_links = drudge_top_headlines.find_all('a') or []
-  splash = top_links.pop() if top_links else None
+  top_links = set(top_links)
+  top_links = top_links.difference(splash)
 
   return {'top': top_links, 'splash': splash}
 
