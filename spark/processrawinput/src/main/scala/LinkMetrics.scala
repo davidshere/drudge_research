a52f88aa202a59d61cf5391419b07ffd161ab5cc
davidshere <sheredlk@gmail.com>
2018-10-14 12:38:33 -0700

    get good working model, I think

--- spark/processrawinput/src/main/scala/LinkMetrics.scala
+++ spark/processrawinput/src/main/scala/LinkMetrics.scala
@@ -12,26 +12,33 @@ import org.apache.spark.sql.functions._
 
 object LinkMetrics {
 
-  def transformInput(df: DataFrame): DataFrame = {
-   
-    // create features
-    val linkIdCountsDf = df.groupBy("linkId").count()
-    
-    val inputWithFeatureColumns = df
-      .select("linkInstanceId", "linkId", "url", "hed")
-      .join(linkIdCountsDf, "linkId")
-      .withColumn("urlCharLength", length(col("url")))
-      .withColumn("hedCharLength", length(col("hed")))
+  def prepareInput(df: DataFrame): DataFrame = {
+    val features = generateFeatures(df) 
 
     val assembler = new VectorAssembler()
+      // current best feature set
       .setInputCols(Array("count", "urlCharLength", "hedCharLength"))
       .setOutputCol("features")
 
-    val dfWithFeatureVector = assembler.transform(inputWithFeatureColumns)
+    val dfWithFeatureVector = assembler.transform(features)
 
     normalizeFeatures(dfWithFeatureVector)
     
   }
+
+  def generateFeatures(df: DataFrame): DataFrame = {
+
+    val linkIdCountsDf = df.groupBy("linkId").count()
+    
+    df
+      .select("linkInstanceId", "linkId", "url", "hed")
+      .join(linkIdCountsDf, "linkId")
+      .withColumn("urlCharLength", length(col("url")))
+      .withColumn("hedCharLength", length(col("hed")))
+      .withColumn("urlhedInteration", length(col("hed")) * length(col("url")))
+      .withColumn("hedUpperPct", levenshtein(col("hed"), lower(col("hed"))) / length(col("hed")))
+  }
+
 
   def normalizeFeatures(df: DataFrame): DataFrame = {
     val normalizer = new Normalizer()
@@ -41,39 +48,63 @@ object LinkMetrics {
 
     normalizer
       .transform(df)
-      .withColumnRenamed("normFeature", "features")
+      .drop("features")
+      .withColumnRenamed("normFeatures", "features")
+  }
+
+  def cleanUpPredictions(df: DataFrame): DataFrame = {
+    /*
+     * Our model is pretty good at figuring out short term links
+     * vs. long term links, but some are miscategorized, and obviously
+     * so. In particular, a long term link should only have uppercase
+     * letters in its headline, but some have lowercase letters. This
+     * function finds links labeled long term and if they satisfy some
+     * conditions, recategorizes them as short term links. It also
+     * turns the 0 and 1 into `s` and `l`
+     */
+    df
+      // recategorize st links as lt if they have lowercase headlines
+      .withColumn(
+        "predictionFixedHedCase",
+	when(
+	  col("prediction") === 0 && 
+	  col("hed") =!= upper(col("hed")) && 
+          ! lower(col("url")).like("%drudge%") &&
+          ! col("url").like("%buycytotec"), 1)
+          .otherwise(col("prediction"))
+       )
+      // change predictions from 0 to 1 to `s` and `l`
+      .withColumn(
+        "linkType",
+        when(col("predictionFixedHedCase") === 0, "l").otherwise("s")
+      )
+      // drop intermediate columns
+      .drop("prediction")
+      .drop("predictionFixedHedCase")
+  
   }
 
   def cleanOutput(df: DataFrame): DataFrame = {
     /*
-    Do a little post processing, dropping unnecessary columns and
-    using meaningful terms for the prediction model outputs. 
-
-    We're also going to filter out predicted long term values
-    that we are no are false based on a simple rule, which is that
-    long term values have only uppercase headlines. Ideally that'd
-    feed into the model but I'm not sure how to convey that informatino
-    to a k-means clustering algorithm
+     * Post processning steps:
+     *   - fix some predictions and make the output meaningful
+     *   - filter down to only the columns we need to return
+     *   - filter down to only distict entries, one row per linkId
     */
-    df
-      .drop("linkId")
-      .drop("count")
-      .drop("features")
-      .withColumn(
-        "linkType",
-        when(col("prediction") === 0, "longTerm")
-          .otherwise("shortTerm"))
-      .drop("prediction")
-     
+    val dfWithCleanPredictions = cleanUpPredictions(df)
+
+    dfWithCleanPredictions
+      .select("linkId", "linkType")
+      .distinct
   }
     
   def clusterLinkTypes(df: DataFrame): DataFrame = {
-    val inDf = transformInput(df)
+    val input = prepareInput(df)
 
     // train the model, apply the clusters to the input data
     val kmeans = new KMeans().setK(2).setSeed(1l)
-    val model = kmeans.fit(inDf)
-    val dfWithPredictions = model.transform(inDf)
+    val model = kmeans.fit(input)
+    val dfWithPredictions = model.transform(input)
 
     // transform and return the output
     cleanOutput(dfWithPredictions)
